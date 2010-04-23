6b2b5d837329900b555dfe70aa62c7c943cd157d
David Granath <dur@musiker.nu>
2010-04-23 15:24:13 +0200

    Done with sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,6 +38,10 @@ class User < ActiveRecord::Base
   def has_password?(submitted_password)
     encrypted_password == encrypt(submitted_password)
   end
+  def remember_me!
+    self.remember_token = encrypt("#{salt}--#{id}")
+    save_without_validation
+  end
 
   def self.authenticate(email, submitted_password)
     user = find_by_email(email)
@@ -48,19 +52,21 @@ class User < ActiveRecord::Base
 
   private
   
-    def encrypt_password      
+  def encrypt_password
+    unless password.nil?
       self.salt = make_salt
       self.encrypted_password = encrypt(password)
     end
-
-    def encrypt(string)
-      secure_hash("#{salt}#{string}")
-    end
-    def make_salt
-      secure_hash("#{Time.now.utc}#{password}")
-    end
-    def secure_hash(string)
-      Digest::SHA2.hexdigest(string)
-    end
-
+  end
+  
+  def encrypt(string)
+    secure_hash("#{salt}#{string}")
+  end
+  def make_salt
+    secure_hash("#{Time.now.utc}#{password}")
+  end
+  def secure_hash(string)
+    Digest::SHA2.hexdigest(string)
+  end
+  
 end
---
+++ spec/views/sessions/new.html.erb_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe "/sessions/new" do
+  before(:each) do
+    render 'sessions/new'
+  end
+
+
+end
